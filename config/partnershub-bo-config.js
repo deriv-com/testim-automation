6d465d1d58224e0bbf173560a1bc3f8994984007
jia-deriv <jiawei@regentmarkets.com>
2024-12-20 18:14:12 +0800

    chore: update

--- config/partnershub-bo-config.js
+++ config/partnershub-bo-config.js
@@ -3,9 +3,8 @@ require("dotenv").config();
 
 exports.config = {
   beforeSuite: function (suite) {
-    console.log("beforeSuite", suite);
-
-    const slackApiUrl = process.env.SLACK_WEBHOOK_URL;
+    const slackApiUrl =
+      "https://hooks.slack.com/triggers/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";
 
     const testim_link = `https://app.testim.io/#/project/${encodeURIComponent(
       process.env.TESTIM_PROJECT
