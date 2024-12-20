d0588b2a4670a1c7d9e685d5116a192865982064
jia-deriv <jiawei@regentmarkets.com>
2024-12-20 11:17:58 +0800

    chore: update workflow with input value

--- config/partnershub-bo-config.js
+++ config/partnershub-bo-config.js
@@ -1,6 +1,31 @@
+const axios = require("axios");
+require("dotenv").config();
+
 exports.config = {
   beforeSuite: function (suite) {
     console.log("beforeSuite", suite);
+
+    const slackApiUrl =
+      "https://hooks.slack.com/triggers/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";
+
+    const testim_link = `https://app.testim.io/#/project/${encodeURIComponent(
+      process.env.TESTIM_PROJECT
+    )}/branch/Jia%2Ftest-webhook/runs/suites/${encodeURIComponent(
+      suite.executionId
+    )}`;
+
+    axios
+      .post(slackApiUrl, {
+        suite: `Test suite started: TestIM Partners Hub Smoke`,
+        testim_link: testim_link,
+        environment: process.env.GITHUB_INPUT_ENVIRONMENT,
+      })
+      .then((response) => {
+        console.log(`Slack API posted successfully: ${response.status}`);
+      })
+      .catch((error) => {
+        console.error(`Error posting to Slack API: ${error}`);
+      });
 
     return {
       g_execution_id: suite.executionId,
