bee6046c2518921e0c31602f75006cf09d34c211
jia-deriv <jiawei@regentmarkets.com>
2024-12-28 00:07:48 +0800

    chore: remove global value setup

--- config/partnershub-bo-config.js
+++ config/partnershub-bo-config.js
@@ -82,17 +82,5 @@ exports.config = {
     } catch (error) {
       console.error("Error posting message:", error);
     }
-
-    return {
-      g_execution_id: suite.executionId,
-      g_login: process.env.G_LOGIN,
-      g_password: process.env.G_PASSWORD,
-
-      g_login_bo: process.env.G_LOGIN_BO,
-      g_password_bo: process.env.G_PASSWORD_BO,
-
-      g_login_non_partner: process.env.G_LOGIN_NON_PARTNER,
-      g_password_non_partner: process.env.G_PASSWORD_NON_PARTNER,
-    };
   },
 };
