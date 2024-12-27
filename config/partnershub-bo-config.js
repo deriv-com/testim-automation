50810b3cc6b87724ad58f70ee2a6f47b8acd5e50
jia-deriv <jiawei@regentmarkets.com>
2024-12-27 21:23:03 +0800

    chore: update to use slack api token

--- config/partnershub-bo-config.js
+++ config/partnershub-bo-config.js
@@ -1,28 +1,75 @@
 const axios = require("axios");
+const { WebClient } = require("@slack/web-api");
 require("dotenv").config();
 
 exports.config = {
-  beforeSuite: function (suite) {
+  beforeSuite: async function (suite) {
     const id = suite.executionId;
     const project = encodeURIComponent(process.env.TESTIM_PROJECT);
     const branch = encodeURIComponent(process.env.BRANCH);
-    const slack = process.env.SLACK_WEBHOOK_URL;
+    const channelId = process.env.SLACK_CHANNEL_ID;
+    const token = process.env.SLACK_TOKEN;
+    const client = new WebClient(token);
 
     const testim_link = `https://app.testim.io/#/project/${project}/branch/${branch}/runs/suites/${id}`;
-
-    axios
-      .post(slack, {
-        app_name: process.env.APP_NAME,
-        suite: process.env.SUITE_NAME,
-        environment: process.env.ENVIRONMENT,
-        testim_link: testim_link,
-      })
-      .then((response) => {
-        console.log(`Slack API posted successfully: ${response.status}`);
-      })
-      .catch((error) => {
-        console.error(`Error posting to Slack API: ${error}`);
+    try {
+      await client.chat.postMessage({
+        channel: channelId,
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "plain_text",
+              text: "App: " + process.env.APP_NAME,
+              emoji: true,
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "plain_text",
+              text: "Published to: " + process.env.ENVIRONMENT,
+              emoji: true,
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "plain_text",
+              text: "Test plan: " + process.env.SUITE_NAME,
+              emoji: true,
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "plain_text",
+              text: "Time: " + new Date().toISOString(),
+              emoji: true,
+            },
+          },
+          {
+            type: "actions",
+            elements: [
+              {
+                type: "button",
+                text: {
+                  type: "plain_text",
+                  text: "TestIM Link",
+                  emoji: true,
+                },
+                value: "click_me",
+                url: testim_link,
+                action_id: "actionId-0",
+              },
+            ],
+          },
+        ],
       });
+      console.log("Message posted successfully");
+    } catch (error) {
+      console.error("Error posting message:", error);
+    }
 
     return {
       g_execution_id: suite.executionId,
